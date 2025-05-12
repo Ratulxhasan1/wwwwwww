933417827eedd52fe60e7c4dc91a852084968666
Ratulxhasan1 <rh060166@gmail.com>
2025-05-12 16:21:25 +0600

    HMm

---
+++ indexx.html/main.js
@@ -0,0 +1,15 @@
+// var biscuitJar;
+// const potato ="lexus";
+// console.log(potato)
+// console.log(biscuitJar)
+// console.log(4 + 5)
+
+
+let pop;
+pop=4;
+if(pop % 2 == 0){
+    console.log("an even number.")
+}
+else{
+    console.log("Odd Number")
+}
